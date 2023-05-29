80970e3ff826b7e38b205d0a56f0488eab6b3eb0
Igor Baiocco <igor64br@gmail.com>
2023-05-29 09:49:42 -0300

    feat: :rocket: arquivos de configuração de formatação

--- src/component/Layout/index.tsx
+++ src/component/Layout/index.tsx
@@ -1,20 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Typography } from "@mui/material";
-import { Storager } from "../../globals/StorageManager";
-import { Outlet, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { Typography } from '@mui/material';
+import { Storager } from '../../globals/StorageManager';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import Navbar from './components/Navbar';
 
 export default function Layout() {
   const { token } = Storager;
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!token.get()) navigate("Login");
+    if (!token.get()) navigate('Login');
   }, []);
 
   return (
     <>
-      <Typography variant="h1">Hello World</Typography>
+      <Navbar />
+
       <Outlet />
     </>
   );
